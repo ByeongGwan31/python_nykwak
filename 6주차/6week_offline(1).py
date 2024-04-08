38c2c2d56a31fedf3790e348bd42e682cc83242f
ByeongGwan <2000bk@naver.com>
2024-04-08 23:33:31 +0900

    + HW02PYP.py

--- 6주차/6week_offline(1).py
+++ 6주차/6week_offline(1).py
@@ -46,40 +46,200 @@
 
 # 4-9. 입력을 받아서 도형 그리기를 해보자.
 
-import turtle
+# import turtle
 
-t = turtle.Turtle()
-t.shape("turtle")
+# t = turtle.Turtle()
+# t.shape("turtle")
 
-s = turtle.textinput("", "도형을 입력하시오 : ")
-if s == '사각형':
-    s = turtle.textinput("", "가로:")
-    w = int(s)
-    s = turtle.textinput("", "세로:")
-    h = int(s)
-    t.forward(w) 
-    t.left(90) 
-    t.forward(h) 
-    t.left(90) 
-    t.forward(w) 
-    t.left(90) 
-    t.forward(h)
-elif s == "삼각형":
-     s = turtle.textinput("", "밑변: ") 
-     w = int(s) 
-     t.forward(w) 
-     t.left(120) 
-     t.forward(w)
-     t.left(120) 
-     t.forward(w)
-elif s == "원":
-    s = turtle.textinput("", "반지름: ") 
-    r = int(s)
-    t.circle(r)
+# s = turtle.textinput("", "도형을 입력하시오 : ")
+# if s == '사각형':
+#     s = turtle.textinput("", "가로:")
+#     w = int(s)
+#     s = turtle.textinput("", "세로:")
+#     h = int(s)
+#     t.forward(w) 
+#     t.left(90) 
+#     t.forward(h) 
+#     t.left(90) 
+#     t.forward(w) 
+#     t.left(90) 
+#     t.forward(h)
+# elif s == "삼각형":
+#      s = turtle.textinput("", "밑변: ") 
+#      w = int(s) 
+#      t.forward(w) 
+#      t.left(120) 
+#      t.forward(w)
+#      t.left(120) 
+#      t.forward(w)
+# elif s == "원":
+#     s = turtle.textinput("", "반지름: ") 
+#     r = int(s)
+#     t.circle(r)
 
-turtle.done()
+# turtle.done()
+
+# for i in "Hello":
+#     print("i =", i)
 
 
+# 5-4.
 
+# for i in [1,2,3,4,5,6,7,8,9]:
+#     print("9 *", i, "=", 9 * i)
 
+# for i in range(9):
+#     print('9 * ', i, '=', 9 * i)
 
+# 5-6.
+
+# for i in range(1, 6, 1):
+#     print(i, end = " ")
+
+# for i in range(10, 0, -1):
+#     print(i, end = " ")
+
+# # lab 5-1
+# import turtle
+# t = turtle.Turtle()
+
+# for count in range(6):
+#     t.circle(100)
+#     t.left(360/6)
+
+# lab 5-2
+# import turtle
+# t = turtle.Turtle()
+# t.shape("turtle")
+
+# # 정삼각형
+# for i in range(3):
+#     t.forward(100)
+#     t.left(360 / 3)
+
+# # 정사각형 그리기 위한 터틀 이동
+# t.penup()
+# t.goto(200, 0)
+# t.pendown()
+
+# # 정사각형 그리기
+# for i in range(4):
+#     t.forward(100)
+#     t.left(360 / 4)
+
+# Lab 5-4
+
+# import turtle
+# t = turtle.Turtle()
+# t.shape("turtle")
+
+# for i in range(6):
+#     t.forward(100)
+#     t.left(360 / 6)     # 360 / 4를 통해 90도 왼쪽으로 틀기
+
+# turtle.done()
+
+# # 도전문제 5-5.
+
+# import turtle
+# t = turtle.Turtle()
+# t.shape("turtle")
+
+# s = turtle.textinput("", "몇각형을 원하시나요? :")
+# n = int(s)
+
+# width = turtle.textinput("", "한 변의 길이는 얼마인가요? :")
+# w = int(width)
+
+# for i in range(n):
+#     t.forward(w)
+#     t.left(360 / n)
+
+# turtle.done()
+
+# # Lab 5-4. 술에 취한 거북이
+
+# import turtle
+# import random
+# t = turtle.Turtle()
+# t.shape("turtle")
+
+# for i in range(30):
+#     length = random.randint(1, 100)
+#     t.forward(length)
+
+#     angle = random.randint(-180, 180)
+#     t.right(angle)
+
+# Lab 5 - 8. 별그리기
+# import turtle
+# t = turtle.Turtle()
+# t.shape("turtle")
+# i = 0
+
+# while i < 5:
+#     t.forward(200)
+#     t.right(144)
+#     i = i + 1
+
+# turtle.done()
+
+# Lab 5-9. 나선형 도형 그리기
+# import turtle
+# t = turtle.Turtle()
+# t.speed(0)
+# t.width(3)
+
+# length = 10
+# while length < 500:
+#     t.forward(length)
+#     t.right(89)
+#     length += 5
+
+# turtle.done()
+
+# Chanllenge 5-9.
+
+# import turtle
+
+# t = turtle.Turtle()
+# # 거북이의 속도는 0으로 설정하면 최대가 된다. 
+# t.speed(0)
+# t.width(3)
+
+# length = 10	  # 초기 선의 길이는 10으로 한다. 
+# # while 반복문이다. 선의 길이가 500보다 작으면 반복한다.  
+# while length < 500:	
+#     t.forward(length)             # length만큼 전진한다. 
+#     t.right(111)                       # 111도 오른쪽으로 회전한다. 
+#     length += 5	        # 선의 길이를 5만큼 증가시킨다.
+# turtle.done()
+
+# lab 5-11.
+
+# import random
+
+# tries = 0
+# guess = 0
+# answer = random.randint(1, 100)
+
+# print('1부터 100까지의 숫자를 맞추시오!')
+
+# while guess != answer:
+#     guess = int(input("숫자를 입력하시오 : "))
+#     tries = tries + 1
+#     if guess < answer:
+#         print("숫자가 낮습니다!")
+#     elif guess > answer:
+#         print("숫자가 높습니다!")
+
+# print(f'축하드립니다! 총 시도횟수 : {tries}')
+
+# 5-10.
+
+st = 'I love Python Programing'
+
+for ch in st:
+    if ch in ['a', 'e', 'i', 'o' , 'u', 'A', 'E', 'I', 'O', 'U']:
+        continue            # 모음일 경우 아래 출력을 건너 뛴다.
+    print(ch, end="")
--- 7주차/HW02PYP.py
+++ 7주차/HW02PYP.py
@@ -2,20 +2,101 @@
 # 3으로 나누어지는지, 혹은 두 정수 모두로도 나누어지는지 알려주는 프로그램을 다음과 같이 작성하시오. 
 # 예를 들어 15와 같은 수가 입력되면 다음 출력 결과와 같은 화면이 나타나면 된다.
 
-number = int(input('정수를 입력하시오 : '))
+# number = int(input('정수를 입력하시오 : '))
 
-if number % 2 == 0:
-    print(f'{number}는 2(으)로 나누어집니다.')
+# if number % 2 == 0:
+#     print(f'{number}는 2(으)로 나누어집니다.')
+# else:
+#     print(f'{number}는 2(으)로 나누어지지 않습니다')
+
+# if number % 3 == 0:
+#     print(f'{number}은 3(으)로 나누어집니다.')
+# else:
+#     print(f'{number}은 3(으)로 나누어지지 않습니다.')
+
+# if number % 2 == 0 and number % 3 == 0:
+#     print(f'{number}은 2와 3(으)로 나누어집니다.')
+# else:
+#     print(f'{number}은 2와 3(으)로 나누어지지 않습니다.')
+
+# 4.5 random.randint(0, 9)를 사용하여 0에서 9사이의 복권번호 3개를 생성하도록 하자.
+# 이들 중에서 3개의 숫자를 모두 맞치면 1억원, 2개를 맞히면 1천만원, 1개를 맞히면 1만원, 모두 틀리면 다음 기회에...
+# 을 출력하는 복권 시스템, 사용자로부터 3개의 정수를 받은 다음 상금을 받는 시스템을 작성
+# 랜덤하게 생성한 복권 당첨 번호가 2, 3, 9라고 가정
+
+# import random
+
+# winNumber = [2, 3, 9]
+
+# userNumber = list(map(int, input("세 복권번호를 입력하시오: ").split()))
+
+# matchCount = 0
+
+# for num in userNumber:
+#     if num in winNumber:
+#         matchCount += 1
+
+# if matchCount == 3:
+#     print('상금 1억원')
+# elif matchCount == 2:
+#     print('상금 1천만원')
+# elif matchCount == 1:
+#     print('상금 1만원')
+# else:
+#     print('다음 기회에...')
+
+# 5-6. 다음과 같이 자동차의 연료 탱크 프로그램을 시뮬레이션하여 보자.
+
+# oil = 500
+
+# while True:
+#     user = input("충전 또는 사용한 연료를 +/- 기호와 함께 입력하시오: ")
+
+#     oil += int(user)
+
+#     print(f'현재 탱크양은 {oil} 입니다.')
+
+#     if oil < 50:
+#         print("경고 : 연료가 10% 미만이니 충전하세요!")
+#         break
+# # 500의 10%는 50이다.
+
+# 5-8. 거꾸로 정수 121이나 3443와 같이 거꾸로 나열해도 그 값이 원래의 값과 같은 정수를 의미
+
+# while True:
+#     num = input("정수를 입력하시오 : ")
+
+#     if num == "-99":
+#         print("프로그램을 종료합니다.")
+#         break
+
+#     if num == num[::-1]:
+#         print(f'{num}은(는) 거꾸로 정수입니다.')
+#     else:
+#         print(f'{num} 은(는) 거꾸로 정수가 아닙니다.')
+
+
+# 5-9. 사용자로부터 임의의 양의 정수를 입력 받아, 다음과 같이 입력된 정수의 개수와, 가장 큰 값, 가장 작은 값을 출력하도록 하자.
+# 입력을 받을 때, -99가 들어올 경우 더이상 입력을 받지 않는다.abs
+
+max_num = None
+min_num = None
+count = 0
+
+while True:
+    num = int(input('정수를 입력하시오 : '))
+
+    if num == -99:
+        break
+
+    if max_num is None or num > max_num:
+        max_num = num
+    if min_num is None or num < min_num:
+        min_num = num
+
+    count = count + 1
+
+if count > 0:
+    print(f'{count}개의 유효한 정수 중 가장 큰 정수는 {max_num} 이고, 가장 작은 정수는 {min_num} 입니다.')
 else:
-    print(f'{number}는 2(으)로 나누어지지 않습니다')
-
-if number % 3 == 0:
-    print(f'{number}은 3(으)로 나누어집니다.')
-else:
-    print(f'{number}은 3(으)로 나누어지지 않습니다.')
-
-if number % 2 == 0 and number % 3 == 0:
-    print(f'{number}은 2와 3(으)로 나누어집니다.')
-else:
-    print(f'{number}은 2와 3(으)로 나누어지지 않습니다.')
-
+    print("정수를 입력해주세요.")
